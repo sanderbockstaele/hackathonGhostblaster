392857c95c4574d9f9ebe6b8d44d2fbca6de254b
sander bockstaele <sander.bockstaele@student.odisee.be>
2024-06-17 13:37:28 +0200

    Rewrite game loop

--- hackathonGhostblaster/ghost.hpp
+++ hackathonGhostblaster/ghost.hpp
@@ -1,7 +1,8 @@
+#include "Arduino.h"
 #include "HardwareSerial.h"
 #include <Servo.h>
 
-#define baseLightLevel 40
+#define baseLightLevel 180
 #define up 100
 #define down 0
 
@@ -16,25 +17,26 @@ Servo servo;
 unsigned long ghostCurrentMillis = 0;
 unsigned long ghostPreviousMillis = 0;
 
-unsigned long hitCurrentMillis = 0;
-unsigned long hitPreviousMillis = 0;
+unsigned long currentShotMilis = 0;
+unsigned long previousShotMilis = 0;
 
 int ghostPin[] = {PB4, PB2, PB3, PB5, PB6};
-int ghostVisible[] = {0, 100, 220, 0, 100};
+int ghostVisible[] = {0, 100, 150, 0, 100};
 int ghostInvisible[] = {100, 10, 50, 80, 0};
-bool ghostStatus[] = {false, false, false, false, false};
+bool ghostStatus[] = {false, false, false, false, false}; //true = up, false = down
 
 int ghostShown = 0;
 
 bool hit(uint8_t pin){
   int ghostId = ghostPin[pin-64];
-  
+  Serial.println(analogRead(pin));
   if (analogRead(pin) > baseLightLevel){
-      if (hitCurrentMillis - hitPreviousMillis >= 100) {
-        hitPreviousMillis = hitCurrentMillis;
-        ghostDown(ghostId);
-        return true;
-      }
+    currentShotMilis = millis();
+    if (currentShotMilis - previousShotMilis > 100){
+      Serial.println("BIEM");
+      ghostDown(ghostId);
+      return true;
+    }
   }
   return false;
 }
